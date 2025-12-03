3a873274faa0e8e6917c76f32e5d07c1654dc07c
M-BEDH <https://github.com/M-BEDH>
2025-12-03 11:51:13 +0100

    routes login signup

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,9 +1,18 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
+import SafeScreen from "../components/SafeScreen";
 
 export default function RootLayout() {
   return (
-  <Stack screenOptions={{ headerShown: true}}>
-  <Stack.Screen name="index" options={{ title: "Home"}} />
-  </Stack>
+    <SafeAreaProvider>
+      <SafeScreen>
+        <StatusBar barStyle="dark-content" />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="auth" />
+        </Stack>
+      </SafeScreen>
+    </SafeAreaProvider>
   );
 }
