9d839545a8d1bdca3741a04681c1eced207f8905
M-BEDH <https://github.com/M-BEDH>
2025-12-03 11:52:30 +0100

    composant View avec un style personnalisé

---
+++ mobile/components/SafeScreen.jsx
@@ -0,0 +1,18 @@
+import { View, StyleSheet } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import COLORS from '../constants/colors';
+
+export default function SafeScreen({ children }) {
+  // Utilisation du hook useSafeAreaInsets pour obtenir les informations de la zone de sécurité
+  const insets = useSafeAreaInsets();
+
+  // Retourne un composant View avec un style personnalisé
+  return <View style={[styles.container, {paddingTop: insets.top}]}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+  },
+});
